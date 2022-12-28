e34e6a1d388ae0dc815f9b8639c9b9b56cdbf6b0
David Alger <davidmalger@gmail.com>
2022-12-28 15:17:21 -0600

    2022 Day02

---
+++ 2022/src/day02.rs
@@ -0,0 +1,77 @@
+use utils::prelude::*;
+
+#[derive(Clone)]
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl From<u8> for Play {
+    fn from(play: u8) -> Self {
+        match play {
+            b'A' => Play::Rock,
+            b'B' => Play::Paper,
+            b'C' => Play::Scissors,
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub fn part1(i: String) -> u32 {
+    let mut score = 0;
+    for l in i.lines() {
+        score += play(l.as_bytes()[0].into(), (l.as_bytes()[2] - b'X' + b'A').into());
+    }
+    score
+}
+
+pub fn part2(input: String) -> u32 {
+    let mut score = 0;
+
+    for l in input.lines() {
+        let p1 = l.as_bytes()[0].into();
+        let p2 = match l.as_bytes()[2].into() {
+            // Lose
+            'X' => match p1 {
+                Play::Rock => Play::Scissors,
+                Play::Paper => Play::Rock,
+                Play::Scissors => Play::Paper,
+            },
+            // Draw
+            'Y' => p1.clone(),
+            // Win
+            'Z' => match p1 {
+                Play::Rock => Play::Paper,
+                Play::Paper => Play::Scissors,
+                Play::Scissors => Play::Rock,
+            },
+            _ => unreachable!(),
+        };
+
+        score += play(p1, p2);
+    }
+    score
+}
+
+fn play(p1: Play, p2: Play) -> u32 {
+    let outcome = match (&p1, &p2) {
+        // Win
+        (Play::Rock, Play::Paper) => 2,
+        (Play::Paper, Play::Scissors) => 2,
+        (Play::Scissors, Play::Rock) => 2,
+        // Lose
+        (Play::Rock, Play::Scissors) => 0,
+        (Play::Paper, Play::Rock) => 0,
+        (Play::Scissors, Play::Paper) => 0,
+        _ => 1, // Draw
+    };
+    (p2 as u32 + 1) + (outcome * 3)
+}
+
+tests!(
+    part1_sample(part1(input!("sample")), 15),
+    part1_puzzle(part1(input!("puzzle")), 9177),
+    part2_sample(part2(input!("sample")), 12),
+    part2_puzzle(part2(input!("puzzle")), 12111),
+);
