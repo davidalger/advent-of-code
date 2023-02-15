6e75bb40a43a2f7f038d5138481abfda39e8bbbd
David Alger <davidmalger@gmail.com>
2023-02-14 18:14:40 -0600

    2022 Day16 - Add debug timers

--- utils/src/prelude.rs
+++ utils/src/prelude.rs
@@ -1,6 +1,7 @@
 pub use crate::{input, parse, test, tests};
 pub use itertools::EitherOrBoth::{Both, Left, Right};
 pub use itertools::Itertools;
+pub use log::debug;
 pub use std::cmp::Ordering::{self, Equal, Greater, Less};
 pub use std::collections::hash_map::Entry::Vacant;
 pub use std::collections::*;
