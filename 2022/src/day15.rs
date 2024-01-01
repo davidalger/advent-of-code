c9abcbb464daabe857d313e141ae51730acff63f
David Alger <davidmalger@gmail.com>
2024-01-01 13:43:21 -0600

    2022 Day15 - Encode extra params into solution

--- 2022/src/day15.rs
+++ 2022/src/day15.rs
@@ -1,13 +1,7 @@
 use itertools::Itertools;
 use sscanf::sscanf;
 
-pub struct Input {
-    part1_row: i32,
-    part2_bound: i32,
-    data: Vec<Data>,
-}
-
-struct Data {
+pub struct Data {
     sensor: Pos,
     beacon: Pos,
     range: u32,
@@ -16,39 +10,35 @@ struct Data {
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct Pos(i32, i32);
 
-impl From<String> for Input {
-    fn from(input: String) -> Self {
-        let part1_row = input.lines().find_map(|l| sscanf!(l, "part1_row={i32}").ok()).unwrap();
-        let part2_bound = input.lines().find_map(|l| sscanf!(l, "part2_bound={i32}").ok()).unwrap();
-
-        let data = input
-            .lines()
-            .filter_map(|l| {
-                if let Ok((sx, sy, bx, by)) =
-                    sscanf!(l, "Sensor at x={i32}, y={i32}: closest beacon is at x={i32}, y={i32}")
-                {
-                    let sensor = Pos(sx, sy);
-                    let beacon = Pos(bx, by);
-                    Some(Data { sensor, beacon, range: distance(sensor, beacon) })
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        Self { part1_row, part2_bound, data }
-    }
-}
+utils::parse!(|i| -> Vec<Data> {
+    i.lines()
+        .filter_map(|l| {
+            if let Ok((sx, sy, bx, by)) =
+                sscanf!(l, "Sensor at x={i32}, y={i32}: closest beacon is at x={i32}, y={i32}")
+            {
+                let sensor = Pos(sx, sy);
+                let beacon = Pos(bx, by);
+                Some(Data { sensor, beacon, range: distance(sensor, beacon) })
+            } else {
+                None
+            }
+        })
+        .collect()
+} as Input);
 
 pub fn part1(input: Input) -> usize {
-    let min_x = input.data.iter().map(|data| data.sensor.0 - data.range as i32).min().unwrap();
-    let max_x = input.data.iter().map(|data| data.sensor.0 + data.range as i32).max().unwrap();
+    let min_x = input.iter().map(|data| data.sensor.0 - data.range as i32).min().unwrap();
+    let max_x = input.iter().map(|data| data.sensor.0 + data.range as i32).max().unwrap();
 
     let mut covered = vec![false; min_x.abs_diff(max_x) as usize];
 
-    let y = input.part1_row;
+    let y = match input.iter().map(|data| data.sensor.0.max(data.sensor.1)).max().unwrap() {
+        x if x >= 1000000 => 2000000, // puzzle
+        _ => 10,                      //sample
+    };
+
     for x in min_x..max_x {
-        for data in &input.data {
+        for data in input.iter() {
             let beacon = Pos(x, y);
             if distance(data.sensor, beacon) <= data.range {
                 covered[(x + min_x.abs()) as usize] = true;
@@ -58,21 +48,20 @@ pub fn part1(input: Input) -> usize {
     }
 
     covered.iter().filter(|v| **v).count()
-        - input
-            .data
-            .iter()
-            .filter(|data| data.beacon.1 == y)
-            .map(|data| data.beacon)
-            .unique()
-            .count()
+        - input.iter().filter(|data| data.beacon.1 == y).map(|data| data.beacon).unique().count()
 }
 
 pub fn part2(input: Input) -> u64 {
-    for y in 0..=input.part2_bound {
+    let bound = match input.iter().map(|data| data.sensor.0.max(data.sensor.1)).max().unwrap() {
+        x if x >= 1000000 => 4000000, // puzzle
+        _ => 20,                      //sample
+    };
+
+    for y in 0..=bound {
         let mut x = 0;
-        'x: while x <= input.part2_bound {
+        'x: while x <= bound {
             let beacon = Pos(x, y);
-            for data in &input.data {
+            for data in input.iter() {
                 if distance(data.sensor, beacon) <= data.range {
                     x = data.sensor.0 + (data.range - data.sensor.1.abs_diff(y)) as i32 + 1;
                     continue 'x;
