2c19ef354040b9153c97677e62267e15bf8f93fd
David Alger <davidmalger@gmail.com>
2024-01-02 14:12:01 -0600

    Add ability to fetch some sample inputs via script

--- fetch.sh
+++ fetch.sh
@@ -9,9 +9,22 @@ YEAR=$1
 DAYS=${2:-"$(for i in $(seq 1 25); do echo $i; done)"}
 
 for DAY in ${DAYS}; do
-    echo "Fetching day ${DAY} of year ${YEAR}"
+    puzzle="input/${YEAR}/day$(printf "%02d" "${DAY}")-puzzle.txt"
+    sample="input/${YEAR}/day$(printf "%02d" "${DAY}")-sample.txt"
+
+    echo "Fetching day ${DAY} of year ${YEAR} (sample)"
+    curl -s https://adventofcode.com/${YEAR}/day/${DAY} \
+        -H "cookie: session=$(cat session.txt)" \
+        | sed -n '/<pre><code>/,/<\/code><\/pre>/p' \
+        | grep -m1 -B100 '</code></pre>' \
+        | sed -e 's/<pre><code>//' \
+        | sed '$d' \
+        > "${sample}"
+    sleep 1
+
+    echo "Fetching day ${DAY} of year ${YEAR} (puzzle)"
     curl -s https://adventofcode.com/${YEAR}/day/${DAY}/input \
         -H "cookie: session=$(cat session.txt)" \
-        -o "input/${YEAR}/day$(printf "%02d" "${DAY}")-puzzle.txt"
+        -o "${puzzle}"
     sleep 1
 done
